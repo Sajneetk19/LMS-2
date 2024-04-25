377665d42644bc7c7658d24c9059e89c2cff0448
sandeepk1507 <101385533+sandeepk1507@users.noreply.github.com>
2024-04-25 12:32:33 +0530

    MainPage.jsx

--- MainPage.jsx
+++ MainPage.jsx
@@ -1,30 +1,4 @@
-// import React from 'react';
-// import './MainPage.css'; // import the CSS file
 
-// const MainPage = () => {
-//   return (
-//     <div className="container">
-//       <div className="content-container">
-//         <div className="form-container">
-//           <div className="image-and-buttons-container">
-//             <img src="https://img.freepik.com/premium-vector/student-woman-with-laptop-studying-online-course-online-education-concept-vector-illustration-flat_186332-1147.jpg" alt="Study" />
-//             <div className="button-container">
-//               <button className="login-button">Student Login</button>
-//               <button className="login-button">Teacher Login</button>
-//             </div>
-//           </div>
-//           <div className="text-container">
-//             <h2>Welcome to the Study Hub</h2>
-//             <p>The ultimate platform for students and educators to connect, learn, and grow.</p>
-//             <a href="/" className="blue-link">Learn More</a>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default MainPage;
 import React from 'react';
 import './MainPage.css';
 
@@ -36,7 +10,9 @@ function MainPage() {
         <button className="button">Login as a Student</button>
         <button className="button">Login as a Admin</button>
       </div>
-      <div className="text-container"> <h2>Welcome to the Eduverse</h2> <p>Connect with educators and students from around the world.</p>
+      <div className="text-container"> 
+      <p style={{ fontSize: 50 , fontWeight: 700}}>Welcome to the Eduverse</p>
+
       </div>
     </div>
   );
